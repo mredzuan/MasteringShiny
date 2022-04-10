79f09899f9c58a6909a6378a0545462dcd355949
redz <redzu.m@gmail.com>
2022-04-11 06:28:11 +0800

    Done Chap3 (Basic reactivity)

---
+++ RCode/Ch3_6_ObserveEvent.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, session) {
+  string <- reactive(paste0("Hello ", input$name, "!"))
+  
+  output$greeting <- renderText(string())
+  
+  
+  observeEvent(input$name, {
+    message("Greeting performed ", "{Inputted Name: ", input$name, "}")
+  })
+}
+
+shinyApp(ui, server)
